9d3af27ff0ecb49e1ffeb1d0dcc7e06b2bf37bfe
hoàng0809 <91303390+luongviethoang@users.noreply.github.com>
2022-04-23 14:58:06 +0700

    Update StudentArrayList.java

--- StudentArrayList.java
+++ StudentArrayList.java
@@ -1,17 +1,21 @@
 package btjava1;
 
-import java.util.*;
+import javax.naming.Name;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Comparator;
+import java.util.Collections;
 
  class Student{
     private String Name;
     private Integer Age;
     private int RollNo;
-    private int Dob;
+    private String Dob;
     private String Address;
     private String Email;
     private String Status;
 
-    public Student(String Name, Integer Age, int RollNo, int Dob, String Address, String Email, String Status){
+    public Student(String Name, Integer Age, int RollNo, String Dob, String Address, String Email, String Status){
         this.Name=Name;
         this.Age=Age;
         this.RollNo=RollNo;
@@ -23,7 +27,7 @@ import java.util.*;
     public String getName(){return Name;}
      public int getAge(){return Age;}
      public int getRollNo(){return RollNo;}
-     public int getDob(){return Dob;}
+     public String getDob(){return Dob;}
      public String getAddress(){return Address;}
      public String getEmail(){return Email;}
      public String getStatus(){return Status;}
@@ -31,7 +35,7 @@ import java.util.*;
      public void setName(String Name){this.Name=Name;}
      public void setAge(int Age){this.Age=Age;}
      public void setRollNo(int Roll){this.RollNo=RollNo;}
-     public void setDob(int Dob){this.Dob=Dob;}
+     public void setDob(String Dob){this.Dob=Dob;}
      public  void setAddress(String Address){this.Address=Address;}
 
      public void setEmail(String email) {this.Email=Email;}
@@ -39,17 +43,20 @@ import java.util.*;
 
      @Override
      public String toString(){
-        return "{" + "RollNo= " +RollNo+" FullName= "+Name+" Age= "+Age+
-                " Dob= "+Dob+" Email ="+Email+" Address= "+ Address+
+        return "{" + "RollNo=" +RollNo+" FullName= "+Name+" Age= "+Age+
+                " Dob= "+Dob+" Email= "+Email+" Address= "+ Address+
                 " Status: "+Status+"}";
      }
  }public class StudentArrayList {
      public static void main (String[] args){
          List<Student> Students = new ArrayList<>();
-         Students.add(new Student("Luong viet hoang",18,1,28/9/2003,"Street123","hoangckhg123@gmail.com","normal"));
-         Students.forEach(student -> {
-             System.out.println(Students);
-         });
+         Students.add(new Student("Luong viet hoang",18,1,"2003/9/28","Street123","hoangckhg123@gmail.com","normal"));
+         Students.add(new Student("Nguyen Ba Kha",18,2,"2003/3/32","Street123","hoangckhg123@gmail.com","normal"));
+         Students.add(new Student("Kien Manh Nguyen",18,3,"2003/23/2","Street123","hoangckhg123@gmail.com","normal"));
+         Students.add(new Student("Luong viet Minh",18,4,"2003/5/2","Street123","hoangckhg123@gmail.com","normal"));
+         Students.add(new Student("Dinh Quan Anh",18,5,"2003/4/23","Street123","hoangckhg123@gmail.com","normal"));
+         Collections.sort(Students, Comparator.comparing(Student::getName));
+         System.out.println(Students);
      }}
 
 
